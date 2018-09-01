edeac76b25795c5874ade192a5950433d98e9912
Rachel Wyatt <rw126@evansville.edu>
2018-09-01 19:07:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |name|
+    new_array << "Hello, my name is #{name}."
+  end
+  return new_array
+end
+
+def assign_rooms(input_array)
+  room_array = []
+  input_array.each_with_index do |name, index|
+    indexplusone = index + 1
+  room_array << "Hello, #{name}! You'll be assigned to room #{indexplusone}!"
+end
+return room_array
+end
+
+def printer(batch_badge_creator)
+  batch_badge_creator.map! do |attendees|
+    prints attendees
+  end
+  assign_rooms.map! do |attendees, index|
+    indexplusone = index + 1
+    puts "Hello, #{name}! You'll be assigned to room #{indexplusone}!"
+  end
+end
